b0698bc4476e1596c059d08d0110339894742a8d
Lubanzi-Mnqobi-Ngcobo <ST10504138@myemeris.edu.za>
2026-03-30 12:07:16 +0200

    Social Spark Application

---
+++ app/src/main/java/com/example/socialsparkapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.socialsparkapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnSuggest = findViewById<Button>(R.id.btnSuggest)
+        val btnReset = findViewById<Button>(R.id.btnReset)
+        val edtTimeOfDay = findViewById<EditText>(R.id.edtTimeOfDay)
+        val ansBubble = findViewById<TextView>(R.id.ansBubble)
+
+        btnSuggest. setOnClickListener {
+            // Once the user input is in, fetch it and clean it up
+            val input = edtTimeOfDay.text.toString().lowercase().trim()
+
+
+            // Determine suggestion based on input
+             val result = when (input) {
+                "morning" -> "Send a 'Good Morning' text to a family member"
+                "mid-morning" -> "Reach out to a colleague with a quick 'Thank You'"
+                "afternoon" -> "Share a funny meme or interesting link with a friend"
+                "dinner" -> "Call a friend or relative for a 5-minute catch-up"
+                "night" -> "Leave a thoughtful comment on a friend's post"
+                else -> "Nice Attempt, Please try again with suggestions :) (morning, mid-morning, afternoon snack time, afternoon, dinner, night)"
+            }
+
+            // Display the result
+            ansBubble.text = result
+        }
+
+        btnReset.setOnClickListener {
+            // Clear the input and output
+            edtTimeOfDay.text.clear()
+            ansBubble.text = ""
+        }
+
+    }
+}
